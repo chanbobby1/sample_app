b4bfffd5edee79e33f6e5a612c67588c17ed0787
Bobby Chan <chan.bobby1@gmail.com>
2013-05-06 00:42:19 -0400

    Signin and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
   match '/signup', :to => "users#new"
+  match '/signin', to: "sessions#new"
+  match '/signout', to: "sessions#destroy", via: :delete #invoke using HTTP DELETE request
 
   match '/help', :to => "static_pages#help"
 
